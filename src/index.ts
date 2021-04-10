b37464ece2b8dbcea4429da7f6c20bd05e817562
Jakob de Guzman <142328@cloud.rangitoto.school.nz>
2021-04-10 17:33:05 +1200

    V0.2.7 Fixed EventEmitter bug on TS

--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,7 @@ SOFTWARE.
 
 import WebsocketManager from "./WebsocketManager";
 import fetch from "node-fetch";
-import EventEmitter from "events";
+import EventEmitter from "node:events";
 import { EventDM, EventInvite, EventMessage } from "./Events";
 
 export declare interface Client {
