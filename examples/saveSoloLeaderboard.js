4b2d4d94541aab524b6711c18b24f4c4f30150d8
Super Fire <Sup3rFire@users.noreply.github.com>
2021-06-20 11:10:01 +0700

    style(savesololeaderboards): lint the examples

--- examples/saveSoloLeaderboard.js
+++ examples/saveSoloLeaderboard.js
@@ -1,8 +1,8 @@
 // This script will save a TETR.IO Solo leaderboard to "leaderboard.json"
 // Credit to Turbo-baguette
 
-const { TetraChannel } = require('../dist/index')
-const fs = require('fs')
+const { TetraChannel } = require("../dist/index");
+const fs = require("fs");
 
 async function getLeaderBoard(country) {
   const res = await TetraChannel.leaderboards.tetra_league_full(country);
@@ -10,64 +10,63 @@ async function getLeaderBoard(country) {
 }
 
 async function getUserRecords(userId) {
-
   let sprintTime = null;
   let blitzScore = null;
 
   const res = await TetraChannel.users.records(userId);
- 
-  sprintTime = res.data.records["40l"].record.endcontext.finalTime
-  blitzScore = res.data.records.blitz.record.endcontext.score
+
+  sprintTime = res.data.records["40l"].record.endcontext.finalTime;
+  blitzScore = res.data.records.blitz.record.endcontext.score;
 
   return {
-      sprint: sprintTime,
-      blitz: blitzScore
+    sprint: sprintTime,
+    blitz: blitzScore,
   };
 }
 
 async function createSoloLeadderBoard() {
-  var country = "" // Write the country code here
+  const country = ""; // Write the country code here
 
-  let sprintLeaderboard = [];
-  let blitzLeaderboard = [];
+  const sprintLeaderboard = [];
+  const blitzLeaderboard = [];
 
-  const Players = (await getLeaderBoard(country));
+  const Players = await getLeaderBoard(country);
 
   for (const player of Players) {
-      const playerRecords = await getUserRecords(player["_id"]);
-      console.log(`fetched records for player ${player.username}`);
+    const playerRecords = await getUserRecords(player["_id"]);
+    console.log(`fetched records for player ${player.username}`);
 
-      if (playerRecords.sprint) {
-          sprintLeaderboard.push({
-              player: player.username,
-              time: playerRecords.sprint
-          });
-      }
+    if (playerRecords.sprint) {
+      sprintLeaderboard.push({
+        player: player.username,
+        time: playerRecords.sprint,
+      });
+    }
 
-      if (playerRecords.blitz) {
-          blitzLeaderboard.push({
-              player: player.username,
-              score: playerRecords.blitz
-          })
-      }
-
+    if (playerRecords.blitz) {
+      blitzLeaderboard.push({
+        player: player.username,
+        score: playerRecords.blitz,
+      });
+    }
   }
 
   sprintLeaderboard.sort((a, b) => a.time - b.time);
   blitzLeaderboard.sort((a, b) => b.score - a.score);
 
   return {
-      sprintLeaderboard,
-      blitzLeaderboard
-  }
-
+    sprintLeaderboard,
+    blitzLeaderboard,
+  };
 }
 
-createSoloLeadderBoard().then(res => {
-  fs.writeFile("leaderboard.json", JSON.stringify(res), err => {
+createSoloLeadderBoard()
+  .then((res) => {
+    fs.writeFile("leaderboard.json", JSON.stringify(res), (err) => {
       if (err) {
-          console.log(err)
+        console.log(err);
       }
-  });
-  console.log('all done :)');
-}).catch(console.error);
+    });
+    console.log("all done :)");
+  })
+  .catch(console.error);
