731ba9da57f4fa9d8214822505ace3087169a2bd
Jakob <jakob@rumia.moe>
2023-12-15 01:00:59 +1300

    idk why i included node_modules. probably tired

--- node_modules/eslint-plugin-tsdoc/lib/Debug.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class Debug {
-    static log(message: string): void;
-}
-//# sourceMappingURL=Debug.d.ts.map
--- node_modules/eslint-plugin-tsdoc/lib/index.d.ts
+++
@@ -1,9 +0,0 @@
-import * as eslint from 'eslint';
-interface IPlugin {
-    rules: {
-        [x: string]: eslint.Rule.RuleModule;
-    };
-}
-declare const plugin: IPlugin;
-export = plugin;
-//# sourceMappingURL=index.d.ts.map
