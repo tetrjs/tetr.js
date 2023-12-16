ea6179ae5104834d40a978fb6c825481ee72b2e6
Jakob <jakob@rumia.moe>
2023-12-17 00:20:31 +1300

    chore: move options as a type because referenced multiple places

--- src/ws/commands/game.ready.ts
+++ src/ws/commands/game.ready.ts
@@ -1,11 +1,19 @@
 import Game from "../../game/Game";
 import WebSocketManager from "../WebSocketManager";
 
-export default function (
+export default async function (
   { client }: WebSocketManager,
   { data: { players } }: any
 ) {
-  if (client.me) client.room.game = new Game(client.me, players);
+  if (client.me)
+    client.room.game = new Game(
+      client.me,
+      await Promise.all(
+        players.map((player: any) => {
+          return { ...player, user: client.fetchUser(player.userid) };
+        })
+      )
+    );
 
   client.room.emit("start", client.room.game);
 }
