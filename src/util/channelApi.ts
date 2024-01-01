70f2937d536bbc3c3aa134d32f88642ea132d8d9
Super Fire <36259493+Sup3rFire@users.noreply.github.com>
2024-01-01 16:50:45 -0700

    feat: Add caching

--- src/util/channelApi.ts
+++ src/util/channelApi.ts
@@ -1,21 +1,27 @@
 import { APIResponse } from "./types";
 
-export default async function (
-  endpoint: string,
-  method = "GET",
-  body?: any
-): Promise<APIResponse> {
+const cacheSessionID = `SESS-${Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)}`;
+
+const cache: Map<string, any> = new Map();
+
+export default async function (endpoint: string, method = "GET", body?: any): Promise<APIResponse> {
+  if (method == "GET" && !body) {
+    const cacheData = cache.get(endpoint);
+    if (!!cacheData && new Date().getTime() < cacheData.cache.cached_until) return cacheData.data;
+  }
   let response: APIResponse = await (
     await fetch(`https://ch.tetr.io/api${endpoint}`, {
       method,
       body,
       headers: {
         Accept: "application/json",
+        "X-Session-ID": cacheSessionID,
       },
     })
   ).json();
 
   if (!response.success) throw Error(response.error);
 
+  cache.set(endpoint, response);
   return response.data;
 }
