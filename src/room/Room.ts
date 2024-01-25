46dd199df4895fbe03386e5baf56959dbf3c148f
mat1jaczyyy <mat1jaczyyy@gmail.com>
2024-01-25 23:26:32 +0100

    chore: Room bracketSwitch takes Player

--- src/room/Room.ts
+++ src/room/Room.ts
@@ -182,7 +182,7 @@ export default class Room extends EventEmitter {
    */
   public bracketSwitch(
     data: "spectator" | "player",
-    player?: { user: User; bracket: "spectator" | "player" }
+    player?: Member
   ): void {
     if (player)
       return this.ws.send({
